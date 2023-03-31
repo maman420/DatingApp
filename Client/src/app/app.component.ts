550be5feddd9344236274e7904f4958d5b6dc408
maman420 <mamanon3235@gmail.com>
2023-03-31 16:28:32 +0300

    login and register ui

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import User from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'Client';
-  users: any;
 
-  constructor(private http: HttpClient) 
+  constructor(private accountService: AccountService) 
   {}
 
   ngOnInit(): void {
-    this.http.get("http://localhost:5001/api/users").subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("requsest is completed")
-    })
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return; //if the userString is empty it returns false and the return keyword is stopping the function
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 
 }
---
+++ Client/src/app/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import User from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit{
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {}
+
+  constructor(private accountService: AccountService) {
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  cancel() {
+    console.log('canceled');
+    this.cancelRegister.emit(false);
+  }
+
+}
